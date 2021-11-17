03861d55b90813da6349e4492834315ecda92113
Atieno-ouma <atienooumavee@gmail.com>
2021-11-17 12:13:02 +0300

    added logics

--- main.py
+++ main.py
@@ -7,15 +7,50 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
 names = input("Enter names separated by commas: ").title().split(",")
 assignments = input("Enter assignment counts separated by commas: ").split(",")
 grades = input("Enter grades separated by commas: ").split(",")
 
 message = "Hi {},\n\nThis is a reminder that you have {} assignments left to \nsubmit before you can graduate. You're current grade is {} and can increase \nto {} if you submit all assignments before the due date.\n\n"
 
-
 for name, assignment, grade in zip(names, assignments, grades):
-    print(message.format(name, assignment, grade, int(grade) + int(assignment)*2))
+    send =message.format(name, assignment, grade, int(grade) + int(assignment)*2)
+    print(send)
+# Press the green button in the gutter to run the script.
+
+def party_planner(cookies, people):
+    leftovers = None
+    num_each = None
+    # TODO: Add a try-except block here to
+    #       make sure no ZeroDivisionError occurs.
+    try:
+        num_each = cookies // people
+        leftovers = cookies % people
+    except ZeroDivisionError:
+        print("Oops, you entered 0 people will be attending.")
+        print("Please enter a good number of people for a party.")
+
+    return(num_each, leftovers)
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+#try:
+    # some code
+#except Exception as e:
+   # some code
+  # print("Exception occurred: {}".format(e))
+
+f = open('sample.txt', 'r')
+file_data = f.read()
+f.close()
+
+f = open('sample.txt', 'a')
+f.write(send)
+f.close()
 if __name__ == '__main__':
     print_hi('PyCharm')
 
